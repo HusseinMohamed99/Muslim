e8ffe708b90474cb97f57d69cc0bb8389a40f46f
HusseinMohamed99 <husseinhtm99@gmail.com>
2023-01-27 17:09:20 +0200

    save local data Responsive

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:muslim_app/desktop/desktop_screen.dart';
 import 'package:muslim_app/mobile/screens/hadith/hadith_details_screen.dart';
 import 'package:muslim_app/mobile/screens/hadith/hadith_screen.dart';
@@ -29,7 +30,7 @@ void main() async {
             DeviceOrientation.portraitUp,
             DeviceOrientation.portraitDown,
           ]);
-          return  MyApp();
+          return MyApp();
         },
       ),
     ),
@@ -37,7 +38,7 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-   MyApp({super.key});
+  MyApp({super.key});
 
   late SettingsProvider settingsProvider;
 
@@ -46,50 +47,57 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     settingsProvider = Provider.of<SettingsProvider>(context);
     getValueFromPref();
-    return MaterialApp(
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: AppLocalizations.supportedLocales,
-      locale: Locale(settingsProvider.currentLanguage),
+    return ScreenUtilInit(
+        designSize: const Size(360, 690),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) {
+          return MaterialApp(
+            localizationsDelegates: const [
+              AppLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: AppLocalizations.supportedLocales,
+            locale: Locale(settingsProvider.currentLanguage),
 
-      ///OR
-      // supportedLocales: const [
-      //   Locale('en', ''), // English, no country code
-      //   Locale('ar', ''), // Spanish, no country code
-      // ],
-      theme: ThemeApp.lightTheme,
-      darkTheme: ThemeApp.darkTheme,
-      themeMode: settingsProvider.currentTheme,
-      debugShowCheckedModeBanner: false,
-      home: LayoutBuilder(
-          builder: (BuildContext context, BoxConstraints constraints) {
-        if (kDebugMode) {
-          print(constraints.minWidth.toInt());
-        }
-        if (constraints.minWidth.toInt() <= 500) {
-          return const HomeScreen();
-        }
-        return const DesktopScreen();
-      }),
+            ///OR
+            // supportedLocales: const [
+            //   Locale('en', ''), // English, no country code
+            //   Locale('ar', ''), // Spanish, no country code
+            // ],
+            theme: ThemeApp.lightTheme,
+            darkTheme: ThemeApp.darkTheme,
+            themeMode: settingsProvider.currentTheme,
+            debugShowCheckedModeBanner: false,
+            home: LayoutBuilder(
+                builder: (BuildContext context, BoxConstraints constraints) {
+              if (kDebugMode) {
+                print(constraints.minWidth.toInt());
+              }
+              if (constraints.minWidth.toInt() <= 500) {
+                return const HomeScreen();
+              }
+              return const DesktopScreen();
+            }),
 
-      routes: {
-        HomeScreen.routeName: (_) => const HomeScreen(),
-        QuranScreen.routeName: (_) => const QuranScreen(),
-        SuraDetailsScreen.routeName: (_) => const SuraDetailsScreen(),
-        HadithScreen.routeName: (_) => const HadithScreen(),
-        HadithDetailsScreen.routeName: (_) => const HadithDetailsScreen(),
-        SebhaScreen.routeName: (_) => const SebhaScreen(),
-        SettingsScreen.routeName: (_) => const SettingsScreen(),
-        RadioScreen.routeName: (_) => const RadioScreen(),
-        DesktopScreen.routeName: (_) => const DesktopScreen(),
-      },
-      initialRoute: '/',
-    );
+            routes: {
+              HomeScreen.routeName: (_) => const HomeScreen(),
+              QuranScreen.routeName: (_) => const QuranScreen(),
+              SuraDetailsScreen.routeName: (_) => const SuraDetailsScreen(),
+              HadithScreen.routeName: (_) => const HadithScreen(),
+              HadithDetailsScreen.routeName: (_) => const HadithDetailsScreen(),
+              SebhaScreen.routeName: (_) => const SebhaScreen(),
+              SettingsScreen.routeName: (_) => const SettingsScreen(),
+              RadioScreen.routeName: (_) => const RadioScreen(),
+              DesktopScreen.routeName: (_) => const DesktopScreen(),
+            },
+            initialRoute: '/',
+          );
+        });
   }
+
   getValueFromPref() async {
     final pref = await SharedPreferences.getInstance();
     settingsProvider.changeLanguage(pref.getString("Lang") ?? "Light");
