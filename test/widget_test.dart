aa2afa2c3f32f2c6755f5ee8ce45c2d5f58b989b
HusseinMohamed99 <husseinhtm99@gmail.com>
2025-01-26 14:37:21 +0200

    Refactor main.dart and update dependencies
    Moved the `MuslimApplication` class to a new file `muslim_app.dart` and updated the import paths accordingly. Also, updated various package versions in `pubspec.lock` to their latest versions.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:muslim_app/main.dart';
+import 'package:muslim_app/muslim_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
